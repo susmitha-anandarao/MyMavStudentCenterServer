87c23e0e79e30dfaa6cc87bb38fbb73a9b41a9d7
Susmitha <saphire.susmitha@gmail.com>
2015-07-10 12:40:23 -0500

    check application status and search updated

---
+++ check_application_status.php
@@ -0,0 +1,38 @@
+<?php
+/*
+	* This page is used to check application status
+*/
+require "config.php";
+	//netid is retrieved from client
+    if (isset($_POST['netid'])){
+        $response_array = array();
+        $netid = $_POST['netid'];
+        //dept_no is retrieved
+		$stmt = $conn->prepare("select type from students where netid = ?");
+		$stmt->execute(array($netid));
+		while ($row = $stmt->fetch()) {
+		    $type = $row['type'];
+        }
+        if($type == "P"){
+        	$stmt = $conn->prepare("select application_number,application_name,application_status,application_date from application_status where netid = ?");
+			$stmt->execute(array($netid));
+			while ($row = $stmt->fetch()) {
+				$response = array();
+		    	$response['application_number'] = $row['application_number'];
+				$response['application_name'] = $row['application_name'];
+				$response['application_status'] = $row['application_status'];
+				$response['application_date'] = $row['application_date'];
+				array_push($response_array,$response);
+        	}
+        }
+        
+        if(isset($response_array)){
+            echo json_encode($response_array);
+        }
+        else{
+            $response = array();
+            $response['success'] = 0;
+            echo json_encode($response);
+        }
+    }
+?>
